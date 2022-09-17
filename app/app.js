60fa547b2eaca842f0f8cd553953c2a7c4295fcf
Claire <claralainez@yahoo.es>
2022-09-17 16:18:24 -0300

    commit mdoulo 2

---
+++ app/app.js
@@ -0,0 +1,2 @@
+var test = 'Hola';
+console.log(test);
---
+++ example01.ts
@@ -0,0 +1,4 @@
+let a: number; //*Interferencia explicita
+let b: string; //*Interferencia explicita
+let c=101;     //*Interferencia implicita
+let recursos: string[]=["memoria", "disco", "procesador"]
